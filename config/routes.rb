aca676a89c16f4be2c65aec339d6a73683c29548
NgDucTung <nguyen.duc.tung@framgia.com>
2014-05-14 08:46:37 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Fels7::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
